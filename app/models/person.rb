39369af50e278517e6f4a1ce40b8ad4e97210cf8
David Dang <dtudang@gmail.com>
2014-12-17 16:52:27 -0800

    completed

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,5 +1,9 @@
 class Person < ActiveRecord::Base
-
+  has_many :employments
+  has_many :organizations, through: :employments
+  has_many :locations, through: :employments
+  has_many :job_titles, through: :employments
+  
   def full_name
     "#{first_name} #{last_name}"
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   root to: "employments#index"
   resources :employments
+  resources :people
+  resources :locations
+  resources :organizations
+  resources :job_titles
 end
